785d0c62c18d71de4b3481358978f3e8c35b359f
Tlaloc-Es <Tlaloc-Es@users.noreply.github.com>
2018-06-19 10:37:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,22 +2,19 @@ const fs = require('fs');
 
 module.exports = {
   split: function(fileIn, fileOut, numLines, callback) {
-    
     var fileArray = []
     var lines = require('fs').readFileSync(fileIn, 'utf-8')
     .split('\n')
     .filter(Boolean);
+    
     var counterFile = 0
     var counterLines = 0
-    
-    for (i = 0; i < lines.length; i++) {
+    for (i = 0; i < lines.length; i++) { 
       counterLines++
       fileName = fileOut + String(counterFile)
-      
       fs.appendFileSync(fileName, lines[i], function (err) {
         if (err) {throw err}
       })
-      
       if(i != lines.length || counterLines != numLines){
         fs.appendFileSync(fileName, "\n", function (err) {
           if (err) {throw err}
@@ -29,11 +26,14 @@ module.exports = {
         counterFile++
         counterLines = 0
       }
-      
+
+      if(i==(lines.length-1)){
+        fileArray.push(fileName)
+      }
+    
     }
     
     callback(fileArray)
-  
+
   }
 };
-
